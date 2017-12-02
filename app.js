419e8cb6fbc68bb368a42f9ac91190b1d6492582
An Luong <anpluong@gmail.com>
2017-12-02 03:03:31 -0800

    database

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const express = require('express');
 var bodyParser = require('body-parser')
 const appServer = express();
-
+var mysql = require('mysql');
 // appServer.use(bodyParser.text({
 //     type: "text/html"
 // }));
 
+var connection = mysql.createConnection({
+    host        : 'localhost',
+    user        : 'root',
+    password    : 'password1',
+    database    : 'stockfun'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+    
 appServer.use(bodyParser.urlencoded({extended : true}));
 
 appServer.use(express.static('public'));
 
 appServer.post('/', (req, res) => {
     //req.body is an object
+//    connection.query("INSERT INTO mystock(stocksymbol, openprice , currentprice , stockdate) VALUES ('"+req.body.stockSymbol+"','"+req.body.openStockPrice+"','"+req.body.currentStockPrice+"','"+req.body.stockDate+"')");
+connection.query("INSERT INTO mystock(stocksymbol, openprice , currentprice , stockdate) VALUES ('"+req.body.stockSymbol+"','"+req.body.openStockPrice+"','"+req.body.currentStockPrice+"','"+req.body.stockDate+"')");
+  //  console.log(req.body.currentStockPrice);
     res.send(req.body);
 })
 
